9ff84dc64ddfd180abf03be5f05608248031accf
NayeongKim <nayeong-v-kim@gmail.com>
2022-09-15 23:52:42 +0900

    ga

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -13,12 +13,12 @@ const siteMetadata = {
   github: 'https://github.com/nayeong-v-kim',
   linkedin: 'https://linkedin.com/in/nayeong-kim-ab1245106',
   locale: 'en-US',
-  // analytics: {
-  //   // supports plausible, simpleAnalytics or googleAnalytics
-  //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-  //   simpleAnalytics: false, // true or false
-  //   googleAnalyticsId: 'G-9NFQPPPZB0', // e.g. UA-000000-2 or G-XXXXXXX
-  // },
+  analytics: {
+    // supports plausible, simpleAnalytics or googleAnalytics
+    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    simpleAnalytics: false, // true or false
+    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+  },
   comment: {
     // Select a provider and use the environment variables associated to it
     // https://vercel.com/docs/environment-variables
